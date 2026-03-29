bac257d2bc5145926fe80dac0141f9d2f8a63cbb
DeveloperMahak <mgdeveloper02092005@gmail.com>
2026-03-29 14:06:02 +0530

    Menu-icon layout fixed and right side extra-space fixed

--- portfolio.js
+++ portfolio.js
@@ -1,50 +1,56 @@
 /*--------scroll section active link----------*/
-let sections=document.querySelectorAll('section');
-let navlinks=document.querySelectorAll('header nav a');
-window.onscroll=()=>{
-    sections.forEach(sec=>{
-        let top=window.scrollY;
-        let offset=sec.offsetTop-150;
-        let height=sec.offsetHeight;
-        let id=sec.getAttribute('id');
+let sections = document.querySelectorAll("section");
+let navlinks = document.querySelectorAll("header nav a");
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-        if(top>=offset && top<offset+height){
-            navlinks.forEach(links=>{
-                links.classList.remove('active');
-                document.querySelector('header nav a[href='+id +']').classList.add('active');
-            });
-        };
-    });
-};
-    /*---------------STICKY NAVBAR------------*/
-    let header=document.querySelector('header');
-    header.classList.toggle('sticky',window.scrollY>100);
-    /*---------------SCROLL REVEAL-----------*/
-    ScrollReveal({ 
-        distance:'80px',
-        duration:2000,
-        delay:200,
-     });
-     ScrollReveal().reveal('.home-content, heading', { origin:'top'});
-     ScrollReveal().reveal('#image,.services-container .portfolio-box,.contact form', { origin:'bottom'});
-     ScrollReveal().reveal('.home-contact h1,.about-img', { origin:'left' });
-     ScrollReveal().reveal('.home-contact p,.about-content', {origin:'right' });
-     /*---------------TYPED JS-----------*/
-     const typed=new Typed('.multiple-text',{
-        strings :['Full Stack Developer','Web Developer','App Developer','UI/UX Designer'],
-        typeSpeed :70,
-        backSpeed:70,
-       backDelay:1000,
-       loop:true,
-     });
-/*----------------MENU ON NAVBAR -------------*/
-      const menuIcon = document.getElementById('menu-icon');
-  const navbar = document.getElementById('navbar');
-
-  menuIcon.addEventListener('click', () => {
-    navbar.classList.toggle('active');
+    if (top >= offset && top < offset + height) {
+      navlinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href=" + id + "]")
+          .classList.add("active");
+      });
+    }
   });
+};
+/*---------------STICKY NAVBAR------------*/
+let header = document.querySelector("header");
+header.classList.toggle("sticky", window.scrollY > 100);
+/*---------------SCROLL REVEAL-----------*/
+ScrollReveal({
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+ScrollReveal().reveal(".home-content, heading", { origin: "top" });
+ScrollReveal().reveal(
+  "#image,.services-container .portfolio-box,.contact form",
+  { origin: "bottom" },
+);
+ScrollReveal().reveal(".home-contact h1,.about-img", { origin: "left" });
+ScrollReveal().reveal(".home-contact p,.about-content", { origin: "right" });
+/*---------------TYPED JS-----------*/
+const typed = new Typed(".multiple-text", {
+  strings: [
+    "Full Stack Developer",
+    "Web Developer",
+    "App Developer",
+    "UI/UX Designer",
+  ],
+  typeSpeed: 70,
+  backSpeed: 70,
+  backDelay: 1000,
+  loop: true,
+});
+/*----------------MENU ON NAVBAR -------------*/
+const menuIcon = document.getElementById("menu-icon");
+const navbar = document.getElementById("nav-links");
 
-
-
-
+menuIcon.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+});
